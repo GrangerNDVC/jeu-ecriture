abde6ae57f429966fe5f8fccb81df804bdffbded
GrangerNDVC <j.granger@ndvc.org>
2026-06-14 20:22:09 +0200

    Create  storage.js

---
+++ js/core/storage.js
@@ -0,0 +1,6 @@
+const STORAGE_KEYS = { XP:'archives_xp', LEVEL:'archives_level', ... };
+export const Storage = {
+  getXP: () => ...,
+  addReward: (xpGain, coinGain, comboInc=1) => { ... }
+  // ...
+};

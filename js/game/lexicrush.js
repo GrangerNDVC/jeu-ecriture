771d1052c7ba1076637c33c806aaf4c2b6f15f83
GrangerNDVC <j.granger@ndvc.org>
2026-06-14 20:29:02 +0200

    Create lexicrush.js

---
+++ js/game/lexicrush.js
@@ -0,0 +1,8 @@
+import { Storage } from '../core/storage.js';
+import { refreshHUD, showGameContainer } from '../core/ui.js';
+import { playSound, startBackgroundMusic } from '../core/audio.js';
+
+export async function launchLexicRush(containerElement, onReward) {
+  // ... tout le code de LexicRushEvolved adapté
+  // On utilise onReward pour communiquer avec le hub (XP, pièces)
+}

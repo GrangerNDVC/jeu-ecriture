ae79b6213b01d97ad8720839ab9f98f8fee53cea
GrangerNDVC <j.granger@ndvc.org>
2026-06-14 20:28:14 +0200

    Create audio.js

---
+++ js/core/audio.js
@@ -0,0 +1,5 @@
+let audioCtx = null;
+export function initAudio() { ... }
+export function playSound(type) { ... }
+export function startBackgroundMusic() { ... }
+export function toggleMusic() { ... }
